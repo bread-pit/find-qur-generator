0039503f6612692508239bcbae36776ac16d5ec2
bread-pit <tadiqueangelicamae@gmail.com>
2025-07-11 21:43:20 +0800

    Add: background and animation

---
+++ find-qur/src/components/Menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import qrIcon from "../assets/qr-icon.png";
+
+export default function Menu() {
+  return (
+    <div className="menu-card">
+      <img src={qrIcon} className="qr-icon"></img>
+    </div>
+  );
+}
